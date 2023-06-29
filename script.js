6825be151dff67c02de87d3606c555ba4e24e236
IzaacdePaula <106626163+IzaacdePaula@users.noreply.github.com>
2023-06-29 19:41:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let sideMenu = document.querySelector(".hamburger-menu");
+
+sideMenu.addEventListener("click", function(){
+    document.querySelector(".side-bar").classList.toggle("show-menu")
+    document.querySelector(".hamburger-menu").classList.toggle("hamburger-transition")
+})
+
